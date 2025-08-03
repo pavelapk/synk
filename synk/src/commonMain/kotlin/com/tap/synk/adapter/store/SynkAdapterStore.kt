7927771f1d231a134e9188158e2ca2d8d7592d3a
pavelapk <pepdj@mail.ru>
2025-08-03 23:49:17 +0700

    Add dynamic deserialization with namespace adapter resolution

--- synk/src/commonMain/kotlin/com/tap/synk/adapter/store/SynkAdapterStore.kt
+++ synk/src/commonMain/kotlin/com/tap/synk/adapter/store/SynkAdapterStore.kt
@@ -5,13 +5,23 @@ import kotlin.reflect.KClass
 
 class SynkAdapterStore(
     private val lookup: HashMap<KClass<*>, SynkAdapter<Any>> = HashMap(),
+    private val namespaceLookup: HashMap<String, SynkAdapter<Any>> = HashMap(),
 ) {
 
     fun <T : Any> register(clazz: KClass<T>, adapter: SynkAdapter<T>) {
-        lookup[clazz] = (adapter as SynkAdapter<Any>)
+        val anyAdapter = adapter as SynkAdapter<Any>
+        lookup[clazz] = anyAdapter
+        val qualifiedName = clazz.qualifiedName
+            ?: throw IllegalStateException("No qualifiedName for class ${clazz.simpleName}")
+        namespaceLookup[qualifiedName] = anyAdapter
     }
 
     fun <T : Any> resolve(clazz: KClass<T>): SynkAdapter<Any> {
         return lookup[clazz] ?: throw IllegalStateException("No synk adapter found for given class " + clazz.qualifiedName)
+    }
+
+    fun resolve(namespace: String): SynkAdapter<Any> {
+        return namespaceLookup[namespace]
+            ?: throw IllegalStateException("No synk adapter found for given namespace $namespace")
     }
 }
